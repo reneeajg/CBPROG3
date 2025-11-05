402e560a1652a2cea58382979b5a380adeb52162
reneeajg <renee_guerra@dlsu.edu.ph>
2025-11-06 06:21:51 +0800

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,3 +1,16 @@
 public class Bank {
     private String bankName, bankAccNum;
+    
+    public Bank(String name, String accNum){
+        this.bankName = name;
+        this.bankAccNum = accNum;
+    }
+    
+    public String getBankName(){
+        return this.bankName;
+    }
+    
+    public String getBankAccNum(){
+        return this.bankAccNum;
+    }
 }
