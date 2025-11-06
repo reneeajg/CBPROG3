1cb33c0a57544c26df53c823e41dbe403b861f55
reneeajg <renee_guerra@dlsu.edu.ph>
2025-11-07 07:40:38 +0800

    Update Expense.java

--- Expense.java
+++ Expense.java
@@ -2,14 +2,23 @@
 public class Expense {
 
     // attributes
-    private static final String expenseID = "EXP";
+    private static final String EXPENSE_CODE = "EXP";
+    private static int expCounter = 0;
+    private String expenseID;
     private float expenseAmount;
     private String expenseCurrency, expenseRefNum, expenseReceiverAccNo;
     private DateTime expenseDateTime;
     private Category expenseCategory;
     private Bank expenseBank;
 
-    public Expense (String bankName, String bankAccNum, float amount, String currency, String refNum, String receiverAccNo, DateTime dateTime, Category category){
+    private void createExpID(){
+        expCounter++;
+        this.expenseID = EXPENSE_CODE + String.format("%03d", expCounter);
+    }
+
+    public Expense (String bankName, String bankAccNum, float amount, String currency, String refNum, String receiverAccNo,
+                    DateTime dateTime, Category category){
+        createExpID();
         this.expenseBank = new Bank(bankName, bankAccNum);
         this.expenseAmount = amount;
         this.expenseCurrency = currency;
@@ -18,7 +27,9 @@ public class Expense {
         this.expenseCategory = category;
     }
 
-    public Expense (String bankName, String bankAccNum, float amount, String currency, String refNum, String receiverAccNo, DateTime dateTime){
+    public Expense (String bankName, String bankAccNum, float amount, String currency, String refNum, String receiverAccNo,
+                    DateTime dateTime){
+        createExpID();
         this.expenseBank = new Bank(bankName, bankAccNum);
         this.expenseAmount = amount;
         this.expenseCurrency = currency;
@@ -28,6 +39,7 @@ public class Expense {
 
     public Expense (float amount, String currency, DateTime dateTime, Category category){
 
+        createExpID();
         this.expenseAmount = amount;
         this.expenseCurrency = currency;
         this.expenseDateTime = dateTime;
@@ -36,13 +48,14 @@ public class Expense {
 
     public Expense (float amount, String currency, DateTime dateTime){
 
+        createExpID();
         this.expenseAmount = amount;
         this.expenseCurrency = currency;
         this.expenseDateTime = dateTime;
     }
 
-    public static String getExpenseID(){
-        return expenseID;
+    public DateTime getDate(){
+        return expenseDateTime;
     }
 
     public float getExpenseAmount() {
@@ -68,4 +81,33 @@ public class Expense {
     public String getExpenseReceiverAccNo() {
         return expenseReceiverAccNo;
     }
+
+
+    public String getExpenseSummary() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Amount: ").append(expenseAmount).append(" ").append(expenseCurrency).append(" | Date: ")
+                .append(expenseDateTime.getDay())
+                .append("-").append(expenseDateTime.getMonth())
+                .append("-").append(expenseDateTime.getYear());
+
+        if (expenseCategory != null)
+            sb.append(" | Category: ").append(expenseCategory.getCategoryName());
+
+        if (expenseBank != null) {
+            sb.append(" | Bank: ").append(expenseBank.getBankName());
+            sb.append(" | Account: ").append(expenseBank.getBankAccNum());
+        }
+
+        if (expenseRefNum != null)
+            sb.append(" | Ref#: ").append(expenseRefNum);
+
+        if (expenseReceiverAccNo != null)
+            sb.append(" | Receiver: ").append(expenseReceiverAccNo);
+
+        return sb.toString();
+    }
+
+
+
 }
