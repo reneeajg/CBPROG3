91c42235fb29373a64715feda99cac08680051d1
reneeajg <renee_guerra@dlsu.edu.ph>
2025-11-06 21:48:52 +0800

    Update Expense.java

--- Expense.java
+++ Expense.java
@@ -1,12 +1,16 @@
+
 public class Expense {
 
     // attributes
+    private static final String expenseID = "EXP";
     private float expenseAmount;
     private String expenseCurrency, expenseRefNum, expenseReceiverAccNo;
     private DateTime expenseDateTime;
     private Category expenseCategory;
+    private Bank expenseBank;
 
-    public Expense (float amount, String currency, String refNum, String receiverAccNo, DateTime dateTime, Category category){
+    public Expense (String bankName, String bankAccNum, float amount, String currency, String refNum, String receiverAccNo, DateTime dateTime, Category category){
+        this.expenseBank = new Bank(bankName, bankAccNum);
         this.expenseAmount = amount;
         this.expenseCurrency = currency;
         this.expenseReceiverAccNo = receiverAccNo;
@@ -14,4 +18,54 @@ public class Expense {
         this.expenseCategory = category;
     }
 
+    public Expense (String bankName, String bankAccNum, float amount, String currency, String refNum, String receiverAccNo, DateTime dateTime){
+        this.expenseBank = new Bank(bankName, bankAccNum);
+        this.expenseAmount = amount;
+        this.expenseCurrency = currency;
+        this.expenseReceiverAccNo = receiverAccNo;
+        this.expenseDateTime = dateTime;
+    }
+
+    public Expense (float amount, String currency, DateTime dateTime, Category category){
+
+        this.expenseAmount = amount;
+        this.expenseCurrency = currency;
+        this.expenseDateTime = dateTime;
+        this.expenseCategory = category;
+    }
+
+    public Expense (float amount, String currency, DateTime dateTime){
+
+        this.expenseAmount = amount;
+        this.expenseCurrency = currency;
+        this.expenseDateTime = dateTime;
+    }
+
+    public static String getExpenseID(){
+        return expenseID;
+    }
+
+    public float getExpenseAmount() {
+        return expenseAmount;
+    }
+
+    public String getExpenseCurrency() {
+        return expenseCurrency;
+    }
+
+    public String getExpenseRefNum() {
+        return expenseRefNum;
+    }
+
+    public Category getExpenseCategory() {
+        return expenseCategory;
+    }
+
+    public DateTime getExpenseDateTime() {
+        return expenseDateTime;
+    }
+
+    public String getExpenseReceiverAccNo() {
+        return expenseReceiverAccNo;
+    }
 }
