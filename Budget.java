abb5936b89046b8e952e5fcf034af21bed86e1e3
reneeajg <renee_guerra@dlsu.edu.ph>
2025-11-06 21:48:28 +0800

    Update Budget.java

--- Budget.java
+++ Budget.java
@@ -1,68 +1,45 @@
+public class Budget {
 
+    //attributes
+    private static final String budgetID = "B";
+    private float budgetAmt;
+    private DateTime budgetStart, budgetEnd;
+    private Category category;
 
-public class Expense {
-
-    // attributes
-    private float expenseAmount;
-    private String expenseCurrency, expenseRefNum, expenseReceiverAccNo;
-    private DateTime expenseDateTime;
-    private Category expenseCategory;
-    private Bank expenseBank;
-
-    public Expense (String bankName, String bankAccNum, float amount, String currency, String refNum, String receiverAccNo, DateTime dateTime, Category category){
-        this.expenseBank = new Bank(bankName, bankAccNum);
-        this.expenseAmount = amount;
-        this.expenseCurrency = currency;
-        this.expenseReceiverAccNo = receiverAccNo;
-        this.expenseDateTime = dateTime;
-        this.expenseCategory = category;
+    public Budget(float amount, DateTime start, DateTime end, Category category){
+        this.budgetAmt = amount;
+        this.budgetStart = start;
+        this.budgetEnd = end;
+        this.category = category;
     }
 
-    public Expense (String bankName, String bankAccNum, float amount, String currency, String refNum, String receiverAccNo, DateTime dateTime){
-        this.expenseBank = new Bank(bankName, bankAccNum);
-        this.expenseAmount = amount;
-        this.expenseCurrency = currency;
-        this.expenseReceiverAccNo = receiverAccNo;
-        this.expenseDateTime = dateTime;
+    public Budget(float amount, DateTime start, DateTime end){
+        this.budgetAmt = amount;
+        this.budgetStart = start;
+        this.budgetEnd = end;
     }
 
-    public Expense (float amount, String currency, DateTime dateTime, Category category){
-
-        this.expenseAmount = amount;
-        this.expenseCurrency = currency;
-        this.expenseDateTime = dateTime;
-        this.expenseCategory = category;
+    public void setBudgetAmt(float budgetAmt) {
+        this.budgetAmt = budgetAmt;
     }
 
-    public Expense (float amount, String currency, DateTime dateTime){
-
-        this.expenseAmount = amount;
-        this.expenseCurrency = currency;
-        this.expenseDateTime = dateTime;
+    public static String getBudgetID() {
+        return budgetID;
     }
 
-    public float getExpenseAmount() {
-        return expenseAmount;
+    public float getBudgetAmt() {
+        return budgetAmt;
     }
 
-    public String getExpenseCurrency() {
-        return expenseCurrency;
+    public DateTime getBudgetStart() {
+        return budgetStart;
     }
 
-    public String getExpenseRefNum() {
-        return expenseRefNum;
+    public DateTime getBudgetEnd() {
+        return budgetEnd;
     }
 
-    public Category getExpenseCategory() {
-        return expenseCategory;
-    }
-
-    public DateTime getExpenseDateTime() {
-        return expenseDateTime;
-    }
-
-    public String getExpenseReceiverAccNo() {
-        return expenseReceiverAccNo;
+    public Category getCategory() {
+        return category;
     }
 }
-
