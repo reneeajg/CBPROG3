6072346a0e5c4cd1bebd9c445092f35f64a37392
reneeajg <renee_guerra@dlsu.edu.ph>
2025-11-06 21:32:57 +0800

    added constructors plus getter methods

--- Budget.java
+++ Budget.java
@@ -1,22 +1,67 @@
-public class Budget {
+import java.time.LocalDateTime;
 
-    //attributes
-    private float budgetAmt;
-    private DateTime budgetStart, budgetEnd;
-    private Category category;
+public class Expense {
 
-    public Budget(float amount, DateTime start, DateTime end, Category category){
-        this.budgetAmt = amount;
-        this.budgetStart = start;
-        this.budgetEnd = end;
-        this.category = category;
+    // attributes
+    private float expenseAmount;
+    private String expenseCurrency, expenseRefNum, expenseReceiverAccNo;
+    private DateTime expenseDateTime;
+    private Category expenseCategory;
+    private Bank expenseBank;
+
+    public Expense (String bankName, String bankAccNum, float amount, String currency, String refNum, String receiverAccNo, DateTime dateTime, Category category){
+        this.expenseBank = new Bank(bankName, bankAccNum);
+        this.expenseAmount = amount;
+        this.expenseCurrency = currency;
+        this.expenseReceiverAccNo = receiverAccNo;
+        this.expenseDateTime = dateTime;
+        this.expenseCategory = category;
     }
 
-    public void setBudgetAmt(float budgetAmt) {
-        this.budgetAmt = budgetAmt;
+    public Expense (String bankName, String bankAccNum, float amount, String currency, String refNum, String receiverAccNo, DateTime dateTime){
+        this.expenseBank = new Bank(bankName, bankAccNum);
+        this.expenseAmount = amount;
+        this.expenseCurrency = currency;
+        this.expenseReceiverAccNo = receiverAccNo;
+        this.expenseDateTime = dateTime;
     }
 
-    public float getBudgetAmt() {
-        return budgetAmt;
+    public Expense (float amount, String currency, DateTime dateTime, Category category){
+
+        this.expenseAmount = amount;
+        this.expenseCurrency = currency;
+        this.expenseDateTime = dateTime;
+        this.expenseCategory = category;
+    }
+
+    public Expense (float amount, String currency, DateTime dateTime){
+
+        this.expenseAmount = amount;
+        this.expenseCurrency = currency;
+        this.expenseDateTime = dateTime;
+    }
+
+    public float getExpenseAmount() {
+        return expenseAmount;
+    }
+
+    public String getExpenseCurrency() {
+        return expenseCurrency;
+    }
+
+    public String getExpenseRefNum() {
+        return expenseRefNum;
+    }
+
+    public Category getExpenseCategory() {
+        return expenseCategory;
+    }
+
+    public DateTime getExpenseDateTime() {
+        return expenseDateTime;
+    }
+
+    public String getExpenseReceiverAccNo() {
+        return expenseReceiverAccNo;
     }
 }
