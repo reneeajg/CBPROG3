d8a2d6e144ced86c0531a4d2a84c88800ca4710f
reneeajg <renee_guerra@dlsu.edu.ph>
2025-11-07 07:38:44 +0800

    Add files via upload

---
+++ Category.java
@@ -0,0 +1,11 @@
+public class Category {
+    private String categoryName;
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
